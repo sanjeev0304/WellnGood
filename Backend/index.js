03691aff4332421a4872ad9f7dbe7192f0acc7da
Shourya <shouryakorrapati@gmail.com>
2025-06-30 19:49:34 +0530

    User Data API Created

--- Backend/index.js
+++ Backend/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const connectDB = require('./model/modelConfig');
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require('./routes/userRoutes');
 connectDB();
 
 const app = express();
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 
 
